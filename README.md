ba9db115c3fcd7707ab1098f8b98d3801cd9a816
haivudev <haivu1811@gmail.com>
2021-08-19 23:08:01 +0700

    init project NodeJs

---
+++ README.md
@@ -0,0 +1,19 @@
+# NodeJs + ExpressJs + MongoDb - Building an Api like Trello from scratch
+
+## Requirements
+
+* **nodejs >=v14.7.0**
+* **npm >= v6.14.7**
+* **yarn >= v1.19.1**
+
+Clone project and run test
+
+```
+$ git clone https://git_url_clone <project_dir>
+$ cd <project_dir>
+$ yarn install
+$ yarn start
+```
+
+Authors: Ngọc Hải
+Thanks for watching!
---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { mapOrder } from './utilities/sorts.js';
+
+const app = express();
+
+const hostname = 'localhost';
+const port = 4000;
+
+app.get('/', (res, req) => {
+    res.end('<h1>Hello</h1><hr/>');
+})
+
+app.listen(port, hostname, () => {
+    console.log(`Hello ngochaidev, I'm running at ${hostname}:${port} /`)
+})
