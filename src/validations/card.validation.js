024861f0a1c676e3c05d2ffd9991ab2aedabd987
haivudev <haivu1811@gmail.com>
2021-08-22 23:11:45 +0700

    CRUD APIs

--- src/validations/card.validation.js
+++ src/validations/card.validation.js
@@ -3,7 +3,10 @@ import { HttpStatusCode } from '*/utilities/constants';
 
 const creatNew = async (req, res, next) => {
     const condition = Joi.object({
-        title: Joi.string().required().min(3).max(20).trim()
+        boardId: Joi.string().required(),
+        columnId: Joi.string().required(),
+        boardId: Joi.string().required(),
+        title: Joi.string().required().min(3).max(30).trim()
     });
 
     try {
@@ -16,4 +19,4 @@ const creatNew = async (req, res, next) => {
     }
 };
 
-export const BoardValidation = { creatNew };
+export const CardValidation = { creatNew };
